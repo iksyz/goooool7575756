512b8b258a58e6e5189c5f26399171996288cee0
meihosting21-lab <meihosting21@gmail.com>
2026-01-31 03:30:45 +0300

    ads
    assssss

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 
 type GlobalForPrisma = {
     prisma?: PrismaClient;
